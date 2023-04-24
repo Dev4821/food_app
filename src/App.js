ae5cd329588d3b5c19d175eb09bb9a7dd3f194c9
Dev4281 <divyansh.sharma0210@gmail.com>
2023-04-24 16:54:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import Recipe from './component/Recipe';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+const APP_ID = "cd9e53ac";
+const APP_KEY = "592235da3e41db683cc3bfbed6d9b379";
+const [recipes, setRecipes] = useState([]);
+const [search, setSearch] = useState("");
+const [query, setQuery] = useState("chicken");
+useEffect(() => {
+	getRecipes();
+  // eslint-disable-next-line 
+}, [query])
+const getRecipes = async () => {
+	const response = await fetch
+		(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+	const data = await response.json();
+	setRecipes(data.hits);
+	// console.log(data);
+
+};
+const updateSearch = e => {
+	setSearch(e.target.value);
+};
+const getSearch = e => {
+	e.preventDefault();
+	setQuery(search);
+	setSearch("");
+}
+
+return (
+  <>
+  <Navbar/>
+	<div className="App">
+	<form className="search-form" onSubmit={getSearch} >
+		<input className="search-bar" type="text" value={search}
+			onChange={updateSearch} />
+		<button className="search-button" type="submit" >
+			Search
+		</button>
+	</form>
+	<div className="recipes">
+		{recipes.map(recipe => (
+		<Recipe
+			key={recipe.recipe.label}
+			title={recipe.recipe.label}
+			calories={recipe.recipe.calories}
+			image={recipe.recipe.image}
+			ingredients={recipe.recipe.ingredients}
+		/>
+
+		))}
+	</div>
+
+	</div>
+  <Footer/>
+  </>
+);
 }
 
 export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    
+      <div >
+        <div className="card text-center bg-dark text-muted">
+          <div className="card-header ">CONTACT US</div>
+         
+          <div className="card-body">
+            <h5 className="card-title">Amazed With Cuisins</h5>
+            <p className="card-text">Provides you latest recipe here.</p>
+          </div>
+          <div className="card-footer text-muted">
+            Developed By:
+            <a
+              href={"https://github.com/Dev4821"}
+              className="link-light text-decoration-none"
+              target="_blank"
+              rel="noreferrer"
+            >
+               Muskan
+            </a>
+          </div>
+        </div>
+      </div>
+    
+  )
+}
+
+export default Footer
